4f3cc65baa7ebf960be0b9ebb71312ebcaa7e0b2
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-09-01 01:18:42 +0900

    選択した商品をUserクラスのインスタンス変数に代入する仕様に変更

--- greengrocer_class.rb
+++ greengrocer_class.rb
@@ -26,27 +26,31 @@ class Greengrocer
   
   def disp
     puts "商品を選択してください"
-    @products.each.with_index(@id) do |product, id|
+    @products.each do |product|
       puts "#{product.id}.#{product.name}" "(¥#{product.price})"
     end
   end
 
-  def ask_quantity(selected_product)
-    puts "#{selected_product.name}ですね、何個買いますか？"
+  def ask_quantity(user)
+    puts "#{user.selected_product.name}ですね、何個買いますか？"
   end
 end
 
 class User
+  attr_reader :selected_product
+  def initialize(selected_product:)
+    @selected_product = selected_product
+  end
+
   def choose(greengrocer)
-    selected_product = selected_product
     loop do
       print "商品を選択 >"
       selected_products_num = gets.to_i
-      selected_product = greengrocer.products.find{|product| product.id == selected_products_num}
+      @selected_product = greengrocer.products.find{|product| product.id == selected_products_num}
       break if !selected_product.nil?
         puts "#{greengrocer.products.first.id}から#{greengrocer.products.last.id}の数字を入力してください"
     end
-    selected_product
+    @selected_product
   end
 end
 
@@ -58,7 +62,7 @@ product_params1 = [
 # product_params1 の商品を持つ八百屋の開店
 greengrocer1 = Greengrocer.new(product_params1)
 #お客さんの来店
-user = User.new
+user = User.new(selected_product: nil)
 
 adding_products1 = [
   { name: "かぼちゃ", price: 300 },
@@ -70,9 +74,9 @@ greengrocer1.register_product(adding_products1)
 # 商品を表示
 greengrocer1.disp
 # 商品の決定
-selected_product = user.choose(greengrocer1)
+user.choose(greengrocer1)
 # 個数を訊ねる
-greengrocer1.ask_quantity(selected_product)
+greengrocer1.ask_quantity(user)
 
 #八百屋2
 product_params2 = [
@@ -89,6 +93,6 @@ greengrocer2.register_product(adding_products2)
 # 商品を表示
 greengrocer2.disp
 # 商品の決定
-selected_product = user.choose(greengrocer2)
+user.choose(greengrocer2)
 # 個数を訊ねる
-greengrocer2.ask_quantity(selected_product)
+greengrocer2.ask_quantity(user)
